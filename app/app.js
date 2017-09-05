7fcaec30502b8cb095e093e5a2b144a20df87054
zhangweihua1 <rosewithspittle@gmail.com>
2017-09-05 20:55:42 +0900

    First commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myContacts', [
+  'ngRoute'
+
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/contacts'});
+}]);
